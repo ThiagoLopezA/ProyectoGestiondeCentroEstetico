bc9f1e65c8013d6894108f3ccd6801039d9f9b78
Thiago López <87744456+ThiagoLopezA@users.noreply.github.com>
2021-11-17 16:00:58 -0300

    Comienzo del proyecto.
    Se genero un archivo .cpp ya estructurado para poder empezar a trabajarlo.

---
+++ centroEstetico.cpp
@@ -0,0 +1,26 @@
+// Proyecto gestión de centro estetico.
+// Thiago López Alderete, Legajo:
+// Juan Diego Demichelis, Legajo:
+	
+/*<--Bibliotecas-->*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*<--Struct-->*/
+
+
+
+/*<--Prototipos-->*/
+
+
+
+/*<--Main-->*/
+
+int main()
+{
+	
+}
+
+/*<--Funciones-->*/
